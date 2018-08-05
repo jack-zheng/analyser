ce4457a69b0e053f819914323ffb26a620f72bbe
I306454 <jiabin.zheng01@sap.com>
2018-08-05 17:26:58 +0800

    processing svn data migrate

--- app/app/essearch/config.py
+++ app/app/essearch/config.py
@@ -1,6 +1,7 @@
 class Config(object):
-    git_repo='/home/jack/gitStore/selenium'
-    case_folder='/src'
+    git_repo = '/Users/i306454/gitStore/analyser/PLT-User'
+    case_folder = '/src/com/successfactors/test/qray/cases/plt/usermanagement'
     ESSEARCH_URL='localhost:9200'
-    index_type='selenium'
+    index_type='pltuser'
     doc_type='test_case'
+    db_name = 'legacy_cases.db'
--- app/app/essearch/search.py
+++ app/app/essearch/search.py
@@ -4,6 +4,7 @@ from . import gitutil
 from elasticsearch import Elasticsearch
 from .config import Config
 import json
+import sqlite3
 
 
 essearch_bp = Blueprint('essearch', __name__)
@@ -31,10 +32,61 @@ def migrate_record():
     return response
 
 
+def svn_legacy_case_migrate():
+    # get all records from legacy db and store them into a map
+    # map = {
+    #   case_name: {
+    #       author:xxx, create_time: xxx
+    #      }
+    #  }
+    conn = sqlite3.connect(getattr(Config, 'db_name'))
+    cur = conn.cursor()
+    sql = """select file.entry_id, file.file_name, log.date, log.revision, log.author
+                from svn_files_info file,
+                (select * from (select * from svn_log_entry order by revision desc) group by entry_id) log
+            where log.entry_id = file.entry_id;"""
+
+    cur.execute(sql)
+    ret = cur.fetchall()
+    conn.commit()
+    conn.close()
+    cases_map = {}
+    for sub in ret:
+        time = (sub[2]).split('.')[0]
+        case_entry = {"author": sub[4], "create_date": time}
+        cases_map[sub[1]] = case_entry
+
+    return cases_map
+
+
 @essearch_bp.route('/api/clean', methods=['GET'])
 def remove_all_documents():
     ret = delete_essearch_record('author', '*')
     return jsonify(ret)
+
+
+@essearch_bp.route('/api/search/<string:case_name>', methods=['GET'])
+def search_record(case_name):
+    ret = search_essearch_record("file_name", "*%s*" % case_name)
+
+    response = jsonify(ret)
+    return response
+
+
+@essearch_bp.route('/api/test', methods=['GET'])
+def get_first_essearch_record():
+    ret = search_record("BulkImportDemo.java")
+    print(json.loads(ret.data))
+    return "123"
+
+
+def update_essearch_record(id, entry):
+    # pass id, author, create time and update accordingly
+    ret = es.update(index=getattr(Config, 'index_type'),
+                    doc_type=getattr(Config, 'doc_type'),
+                    body={"doc": entry},
+                    id=id)
+    print("update record: %s" % ret)
 
 
 def insert_essearch_record(case):
@@ -48,7 +100,13 @@ def insert_essearch_record(case):
 def delete_essearch_record(field, match):
     return es.delete_by_query(index=getattr(Config, 'index_type'),
                 doc_type=getattr(Config, 'doc_type'),
-                body={"query": {"wildcard": {field: match}}})
+                body={"query": {"wildcard": {field: match}}},
+                ignore=[400, 404])
+
+
+def search_essearch_record(field, match):
+    match = match.lower()
+    return es.search(index=getattr(Config, 'index_type'), body={"query": {"wildcard":{field: match}}})
 
 
 def convert_json_to_obj(json, obj):
