c2d2118f3352b7e8e53ea62f92e76420995c090f
I306454 <jiabin.zheng01@sap.com>
2019-05-23 19:04:55 +0800

    remove deprecated files

--- scripts/config.py
+++
@@ -1,3 +0,0 @@
-git_repo='/Users/i306454/gitStore/analyser/au-usermanagement'
-case_folder='/au-usermanagement-runtimetest/src/main/java/com/successfactors/usermanagement/qray/cases'
-db_path='./../app/app.db'
