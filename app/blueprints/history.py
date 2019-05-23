321750d91c097cd1388314bcf89e3efc8d0f9fc2
I306454 <jiabin.zheng01@sap.com>
2019-05-23 19:53:41 +0800

    encapsulate update action and finish update form

--- app/blueprints/history.py
+++ app/blueprints/history.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from app.forms import UpdateForm
 from app.extensions import db
 from app.models import TestCase
 from datetime import datetime
@@ -14,9 +15,16 @@ import re
 history_bp = Blueprint('history', __name__)
 
 
-@history_bp.route('/', methods=['GET'])
+@history_bp.route('/', methods=['GET', 'POST'])
 def hello():
-    return render_template('history/index.html')
+    form = UpdateForm()
+    if form.validate_on_submit():
+        gap = update_history()
+        return render_template(
+            'history/index.html',
+            form=form,
+            cost=gap)
+    return render_template('history/index.html', form=form)
 
 
 @history_bp.route('/apacheclient', methods=['POST'])
@@ -31,6 +39,83 @@ def apacheclient():
     return jsonify(request.json)
 
 
+def update_history():
+    start = datetime.now()
+    # Steps to update git history to db
+    # 1. clone/pull repo
+    webhookrepo = ''
+    repopath = './au-usermanagement'
+    if not os.path.isdir(repopath):
+        webhookrepo = Repo.clone_from(
+            url=os.getenv('clone_url'), to_path=repopath)
+        logging.warning('Finish Clone!')
+
+    else:
+        webhookrepo = Repo(repopath)
+        webhookrepo.remotes.origin.pull()
+        logging.warning('Finish Pull')
+
+    # 2. clean db data
+    #       * delete all records from test_case table
+    #       * update slqite_sequence table to reset test_case table
+    #       auto increment count
+    count = TestCase.query.delete()
+    logging.warning('Finish Clean DB! %s records been deleted.' % count)
+
+    # 3. update test case info
+    #       * loop git repo, update history of test case to test_case table
+    #       * over write info of case_backup to test_case
+    file_path = []
+    folder_path = repopath + '/au-usermanagement-runtimetest/src/'\
+        'main/java/com/successfactors/usermanagement/qray/cases'
+    logging.warning('folder path: %s ' % folder_path)
+    for root, dirs, files in walk(folder_path):
+        for file in files:
+            file_path.append(os.path.abspath(join(root, file)))
+
+    logging.warning('[%s] records will be inserted' % len(file_path))
+
+    insertrows = []
+    for path in file_path:
+        prefix = path.split('au-usermanagement')[0]
+        commits = list(webhookrepo.iter_commits(paths=path))
+
+        fname = path.split('/')[-1]
+        # the username format is not unique, may be number or just name
+        # so if the name is not id, use email address instead.
+        author = get_user_id(commits[-1])
+        cdate = datetime.fromtimestamp(commits[-1].committed_date)
+        lby = get_user_id(commits[0])
+        ldate = datetime.fromtimestamp(commits[0].committed_date)
+        relative_path = path.replace(prefix, '', 1)
+
+        case = TestCase(file_name=fname,
+                        author=author,
+                        create_date=cdate,
+                        last_update_by=lby,
+                        last_update_time=ldate,
+                        file_path=relative_path)
+        insertrows.append(case)
+
+    db.session.add_all(insertrows)
+    db.session.commit()
+
+    logging.warning('Start Recover SVN History!')
+    sql = '''
+    update
+        test_case
+    set author=(select case_backup.author from case_backup
+    where test_case.file_name=case_backup.file_name),
+        create_date=(select case_backup.create_date from case_backup
+    where case_backup.file_name=test_case.file_name)
+    where EXISTS (SELECT case_backup.file_name
+    FROM case_backup
+    WHERE case_backup.file_name = test_case.file_name)'''
+    db.session.execute(sql)
+    db.session.commit()
+    return (datetime.now() - start).seconds
+
+
 @history_bp.route('/webhook', methods=['GET', 'POST'])
 def webhook():
     '''
@@ -42,80 +127,8 @@ def webhook():
     repo, I leave this interface, and trigger the udpate manually.
     '''
     if request.method == 'POST':
-        # Steps to update git history to db
-        # 1. clone/pull repo
-        webhookrepo = ''
-        repopath = './au-usermanagement'
-        if not os.path.isdir(repopath):
-            webhookrepo = Repo.clone_from(url=os.getenv('clone_url'),
-                                          to_path=repopath)
-            logging.warning('Finish Clone!')
-
-        else:
-            webhookrepo = Repo(repopath)
-            webhookrepo.remotes.origin.pull()
-            logging.warning('Finish Pull')
-
-        # 2. clean db data
-        #       * delete all records from test_case table
-        #       * update slqite_sequence table to reset test_case table
-        #       auto increment count
-        count = TestCase.query.delete()
-        logging.warning('Finish Clean DB! %s records been deleted.' % count)
-
-        # 3. update test case info
-        #       * loop git repo, update history of test case to test_case table
-        #       * over write info of case_backup to test_case
-        file_path = []
-        folder_path = repopath + '/au-usermanagement-runtimetest/src/'\
-            'main/java/com/successfactors/usermanagement/qray/cases'
-        logging.warning('folder path: %s ' % folder_path)
-        for root, dirs, files in walk(folder_path):
-            for file in files:
-                file_path.append(os.path.abspath(join(root, file)))
-
-        logging.warning('[%s] records will be inserted' % len(file_path))
-
-        insertrows = []
-        for path in file_path:
-            prefix = path.split('au-usermanagement')[0]
-            commits = list(webhookrepo.iter_commits(paths=path))
-
-            fname = path.split('/')[-1]
-            # the username format is not unique, may be number or just name
-            # so if the name is not id, use email address instead.
-            author = get_user_id(commits[-1])
-            cdate = datetime.fromtimestamp(commits[-1].committed_date)
-            lby = get_user_id(commits[0])
-            ldate = datetime.fromtimestamp(commits[0].committed_date)
-            relative_path = path.replace(prefix, '', 1)
-
-            case = TestCase(file_name=fname,
-                            author=author,
-                            create_date=cdate,
-                            last_update_by=lby,
-                            last_update_time=ldate,
-                            file_path=relative_path)
-            insertrows.append(case)
-
-        db.session.add_all(insertrows)
-        db.session.commit()
-
-        logging.warning('Start Recover SVN History!')
-        sql = '''
-        update
-            test_case
-        set author=(select case_backup.author from case_backup
-        where test_case.file_name=case_backup.file_name),
-            create_date=(select case_backup.create_date from case_backup
-        where case_backup.file_name=test_case.file_name)
-        where EXISTS (SELECT case_backup.file_name
-        FROM case_backup
-        WHERE case_backup.file_name = test_case.file_name)'''
-        db.session.execute(sql)
-        db.session.commit()
-
-        return "Finish Process Post Request! Git records have updated", 200
+        gap = update_history()
+        return "Git records have updated in %s sec!" % gap, 200
     else:
         return 'Get request triggered', 200
 
